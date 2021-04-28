efef0e71199f40f38cfc4c26a3a767931c42075e
menorziin <williamsilvapppl@gmail.com>
2021-04-28 20:21:03 -0300

    rav: drop libinit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,9 +122,6 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_moto_trinket
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
